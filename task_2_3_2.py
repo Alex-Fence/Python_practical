6a92224318dc61416759f4ffb325de13ba16d790
alex <bokuto@mail.ru>
2022-02-21 22:55:35 +0300

    task_2_3_2.py старт

--- task_2_3_2.py
+++ task_2_3_2.py
@@ -1,5 +1,8 @@
 import xlrd3
 
-
 with xlrd3.open_workbook('trekking2.xlsx') as wb:
+    sheet0 = wb.sheet_by_index(0)
+    sheet1 = wb.sheet_by_index(1)
+    for n_row in range(1, 13):
+        sheet1.cell_value(n_row, 0)
 
